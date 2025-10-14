bf53fb28f12b62f82c61488ce4a9e997a61b10ab
Alessio Ceccarini <alessioceccarini23@icloud.com>
2025-10-14 19:38:43 +0200

    svg circle

--- scripts3.js
+++ scripts3.js
@@ -1,6 +1,6 @@
 // bottone che porta alla pagina di feedback
-const Button = document.getElementById("rateUs");
+const Button = document.getElementById("rateUs")
 Button.addEventListener("click", function () {
-  window.location.href = "feedback_page.html";
-});
-Button.style.cursor = "pointer";
+  window.location.href = "feedback_page.html"
+})
+Button.style.cursor = "pointer"
