142eb45939fb74b7140d26bd09793c8c9e9fa3eb
Alessio Ceccarini <alessioceccarini23@icloud.com>
2025-10-14 13:47:17 +0200

    file js

---
+++ feedback-page.js
@@ -0,0 +1,21 @@
+// LOGICA STELLE FEEDBACK
+
+const stars = document.querySelectorAll("#rating-stars img")
+let selected = 0
+
+stars.forEach((star, index) => {
+  star.addEventListener("mouseover", () => {
+    stars.forEach((s) => s.classList.remove("hovered"))
+    for (let i = 0; i <= index; i++) stars[i].classList.add("hovered")
+  })
+
+  star.addEventListener("mouseout", () => {
+    stars.forEach((s) => s.classList.remove("hovered"))
+  })
+
+  star.addEventListener("click", () => {
+    selected = index
+    stars.forEach((s) => s.classList.remove("selected"))
+    for (let i = 0; i <= selected; i++) stars[i].classList.add("selected")
+  })
+})
